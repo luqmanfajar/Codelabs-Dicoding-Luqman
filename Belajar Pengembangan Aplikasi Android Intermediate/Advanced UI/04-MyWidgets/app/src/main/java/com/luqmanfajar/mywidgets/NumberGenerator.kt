1de13ec9b541bf77fc399beb7951114ea1bbe410
luqmanfajar <fmuhammadluqman@gmail.com>
2022-11-05 20:54:20 +0700

    Update

--- Belajar Pengembangan Aplikasi Android Intermediate/Advanced UI/04-MyWidgets/app/src/main/java/com/luqmanfajar/mywidgets/NumberGenerator.kt
+++ Belajar Pengembangan Aplikasi Android Intermediate/Advanced UI/04-MyWidgets/app/src/main/java/com/luqmanfajar/mywidgets/NumberGenerator.kt
@@ -1,4 +1,10 @@
 package com.luqmanfajar.mywidgets
 
-class NumberGenerator {
+import java.util.*
+
+internal object NumberGenerator {
+    fun generate(max: Int): Int {
+        val random = Random()
+        return random.nextInt(max)
+    }
 }
